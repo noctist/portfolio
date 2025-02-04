842f0ac3ac12b6ccb18e3df9c7b1f92e59ebb166
Zohran Lakdawala <zohranlakdawala@hotmail.com>
2025-02-04 13:37:21 -0500

    update vite.config.js to host on github

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/zohran-lakdawala-portfolio",
+  plugins: [react()], // remove if you're not using react
 })
