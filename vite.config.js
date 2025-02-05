8185d19e2cee96525558e3153294806a8f8d7ee7
Zohran Lakdawala <zohranlakdawala@hotmail.com>
2025-02-04 19:23:26 -0500

    add path to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/zohran-lakdawala-portfolio",
+  base: "/zohran-lakdawala-portfolio",
   plugins: [react()], // remove if you're not using react
 })
